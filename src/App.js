b55ed67d046513a8526bc40767d1c0426f7d9f08
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-06-13 21:03:29 -0700

    completed lab01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+
+import './App.css';
 
 class App extends React.Component {
   render() {
-    return <h1>Gallery of Horns</h1>
+    return (
+      <>
+      <Header/>
+      <Main/>
+      <Footer/>
+      </>
+    );
   }
 }
 
---
+++ src/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render() {
+    return(
+      <article>
+        <h2>{this.props.name}</h2>
+        <img scr='' alt='' title=''></img>
+        <p>Text</p>
+      </article>
+    );
+  }
+}
+
+export default HornedBeast;
