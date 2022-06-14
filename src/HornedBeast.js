cb29025af7c5f282a5e5f8ca862ff896ee025bf1
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-06-14 11:04:43 -0700

    fixed what i missed for full points, added images and description

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 class HornedBeast extends React.Component {
   render() {
-    return(
+    return (
       <article>
-        <h2>{this.props.name}</h2>
-        <img scr='' alt='' title=''></img>
-        <p>Text</p>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
+        <p>{this.props.description}</p>
       </article>
     );
   }
