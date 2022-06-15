d9e3fabf4bb86a81dac9c91977633a4a921a21d0
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-06-14 18:33:31 -0700

    completed lab02, added bootstrap, and completed some css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends React.Component {
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import './HornedBeast.css'
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      faves: 0
+    };
+  };
+
+handleFaves = () => {
+  this.setState({
+
+      faves: this.state.faves + 1,
+      
+  });
+};
+
   render() {
     return (
       <article>
+        <>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
         <p>{this.props.description}</p>
+        <p>❤️ {this.state.faves} Favorites</p>
+        {/* <p onClick={this.handleFaves}>Click here to make your favorite</p> */}
+          
+        <img 
+          src={this.props.imageUrl} 
+          alt={this.props.description} 
+          title={this.props.title}/>
+        
+          <Button variant="outline-primary" onClick={this.handleFaves}>Click Here!</Button>
+
+        
+      </>
       </article>
     );
   }
