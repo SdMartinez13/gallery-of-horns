a5da730928ae6a3c6caa39c159ab010be1f7938c
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-06-15 22:12:32 -0700

    completed lab03 assignment

--- src/App.js
+++ src/App.js
@@ -2,16 +2,54 @@ import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import data from './data.json'
+import Modal from 'react-bootstrap/Modal'
+import SelectedBeast from './SelectedBeast.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      pickedBeast: '',
+      
+
+    };
+  };
+
+  handleOnHide = () => {
+    this.setState({
+      showModal: false
+    });
+  };
+
+  openModal = (beast) => {
+    this.setState({
+      showModal: true,
+      pickedBeast: beast,
+
+    })
+  };
+
+  
+
   render() {
     return (
       <>
-      <Header/>
-      <Main/>
-      <Footer/>
+        <Header/>
+        <Main data= {data}
+          openModal = {this.openModal}
+        />
+        <SelectedBeast
+          showModal = {this.state.showModal}
+          hideModal = {this.handleOnHide}
+          pickedBeast = {this.state.pickedBeast}
+        />
+       
+        <Footer/>
       </>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
-import data from './data.json'
+// import data from './data.json'
 // import App from './App.js';
 
 class Main extends React.Component {
   render() {
-    let beasts = data.map((beast, index) => {
+    let beasts = this.props.data.map((beast, index) => {
       
        return <HornedBeast
         title={beast.title}
         imageUrl={beast.image_url}
         description={beast.description}
+        openModal ={this.props.openModal}
+        beast = {beast}
         key={index}
         />
       
@@ -18,7 +20,7 @@ class Main extends React.Component {
     
     return (
       
-      <main>
+      <main className="d-flex flex-wrap">
 
         {beasts}
         
